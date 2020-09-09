f2e39b3313fc8481c5cf009c61ecbcf06dac736d
Travis Mewborne <tmewborne@hotmail.com>
2020-09-09 14:02:32 -0500

    Added player controls, platforms and coins

---
+++ Assets/CoinBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinBehavior : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collected a Coin!");
+        //if ("Player".Equals(collision.gameObject.tag))
+        //{
+            Destroy(this.gameObject);
+        //}
+    }
+}
---
+++ Assets/Controls.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask groundMask; //Note: serialized field allows the private value to appear in the inspector
+    private CircleCollider2D cc;
+    public float speed = 5f;
+    public float jumpVelocity = 25f;
+    private float jumpForce = 1000f;
+    public Rigidbody2D rb;
+
+    //Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        cc = GetComponent<CircleCollider2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.position = transform.position + new Vector3(horizontalInput * speed * Time.deltaTime, 0, 0);
+
+        if (isGrounded() && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            rb.velocity = Vector2.up * jumpVelocity;
+        }
+    }
+
+    private bool isGrounded() 
+    {
+        RaycastHit2D raycastHit2D = Physics2D.CircleCast(cc.bounds.center,cc.radius,Vector2.down,.1f,groundMask);
+        return raycastHit2D.collider != null;
+    }
+}
